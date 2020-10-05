e609092abc5702dcd99c2db65ba2781e7af5307c
Jonathan Adamson <jonnhy_09@hotmail.com>
2020-10-05 07:28:08 -0500

    parrafo editado

--- src/components/Main.js
+++ src/components/Main.js
@@ -75,11 +75,23 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic03} alt="" />
           </span>
-          <p>
-            I consider myself a resilient, understanding, cheerful and adaptable
-            person to changes. In addition to being collaborative and teaching
-            or collaborating in what is possible. of course self-taught. <br></br>
-            Always looking for new challenges and learning new things.
+          <h4>Jonathan Albert Adamson Gondola Software engineer</h4> <br/>
+
+          <p align="justify">
+            Currently I work as a fullstack Analyst looking at issues of both fronted (angular) and backend (javascript,
+            java and python), implementer of new requirements and problem solver for an international banking entity in
+            Latin America. Addressing communication problems between serveless micro services in systems based on ddd,
+            cqrs and mesh services architectures. I consider myself a resilient, understanding, cheerful and adaptable
+            person to changes. In addition to being collaborative and teaching or collaborating in what is possible of
+            course self-taught. I like performing in what I do, working collaboratively, being recognized and feeling
+            that I add value to each of the places where I work.
+            I started studying a degree in computer science engineering specializing in the area of ​​networks and
+            telecommunications. But once working in this area, I wanted new challenges and to learn other things. So I
+            decided to dive into software development. I started with small projects as an application for comp time and
+            support for existing applications, so I helped my employer implement the CI / CD cycle for two environments
+            (DEV and QA). And in what I am learning I like more and more the area of ​​Devops infrastructures and
+            systems design. so in the future I would like to be an architect of multi-scale solutions for global
+            solutions and maybe set up my startup.
           </p>
           {close}
         </article>
@@ -121,16 +133,6 @@ class Main extends React.Component {
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
               </a>
             </li>
             <li>
